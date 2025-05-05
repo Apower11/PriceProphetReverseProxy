4fcce604b61e2b6e15ab1452a6b0e99529f97d8f
Apower11 <adampower45@hotmail.com>
2025-05-05 23:04:21 +0100

    issue resolved

--- main.js
+++ main.js
@@ -31,6 +31,25 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/historicalcryptopricedata", (req, res) => {  
+  const options = {
+    method: "GET",
+    url: `https://rest.coinapi.io/v1/exchangerate/${req.query.tickerSymbol}/USD/history?period_id=1DAY&limit=10000&time_start=${req.query.start_date}`,
+    headers: {
+      "X-CoinAPI-Key": "55A9326D-650B-4900-9B6B-7D6FC0DDFA4E"
+    },
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
